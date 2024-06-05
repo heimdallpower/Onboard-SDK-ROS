eccc6ab4b2b75dd55fa0c68bee964fe3c599c586
Magnus Berdal <magnus.berdal@heimdallpower.com>
2024-06-05 09:10:24 +0200

    Cleaner

--- dji_sdk/include/dji_sdk/pps_synchronizer.hpp
+++ dji_sdk/include/dji_sdk/pps_synchronizer.hpp
@@ -29,20 +29,15 @@ public:
     const bool pps_fetch_ok{pps_handler_.getLastAssertTime(last_rising_edge_time_SYSTEM, new_pulse_arrived)};
     pulse_arrived_since_prev_flag_ |= new_pulse_arrived;
 
-    std::chrono::nanoseconds time_HARDSYNC_FC;
+    const auto time_HARDSYNC_FC{toChronoNsecs(stamp_HARDSYNC_FC)};
     if (pps_fetch_ok && stamp_HARDSYNC_FC.flag && pulse_arrived_since_prev_flag_)
     {
-      alignment_exists_               = true;
-      pulse_arrived_since_prev_flag_  = false;
-      in_use_rising_edge_time_.SYSTEM = last_rising_edge_time_SYSTEM;
-
-      getFCTimespec(stamp_HARDSYNC_FC, in_use_rising_edge_time_.HARDSYNC_FC);
-      getFCTimespec(stamp_PACKAGE_FC, in_use_rising_edge_time_.PACKAGE_FC);
-
-      time_HARDSYNC_FC = in_use_rising_edge_time_.HARDSYNC_FC;
+      alignment_exists_                     = true;
+      pulse_arrived_since_prev_flag_        = false;
+      in_use_rising_edge_time_.SYSTEM       = last_rising_edge_time_SYSTEM;
+      in_use_rising_edge_time_.HARDSYNC_FC  = time_HARDSYNC_FC;
+      in_use_rising_edge_time_.PACKAGE_FC   = toChronoNsecs(stamp_PACKAGE_FC);
     }
-    else
-      getFCTimespec(stamp_HARDSYNC_FC, time_HARDSYNC_FC);
 
     const auto time_SYSTEM{pps::getSystemTime(time_HARDSYNC_FC, in_use_rising_edge_time_.HARDSYNC_FC, in_use_rising_edge_time_.SYSTEM)};
     pps::chrono2secnsec(time_SYSTEM, time_SYSTEM_out.sec, time_SYSTEM_out.nsec);
@@ -51,8 +46,7 @@ public:
 
   bool getSystemTime(const DJI::OSDK::Telemetry::TimeStamp& stamp_PACKAGE_FC, ros::Time& time_SYSTEM_out)
   {
-    std::chrono::nanoseconds time_PACKAGE_FC;
-    getFCTimespec(stamp_PACKAGE_FC, time_PACKAGE_FC);
+    const auto time_PACKAGE_FC{toChronoNsecs(stamp_PACKAGE_FC)};
     const auto time_SYSTEM{pps::getSystemTime(time_PACKAGE_FC, in_use_rising_edge_time_.PACKAGE_FC, in_use_rising_edge_time_.SYSTEM)};
     pps::chrono2secnsec(time_SYSTEM, time_SYSTEM_out.sec, time_SYSTEM_out.nsec);
     return alignment_exists_;
@@ -70,7 +64,7 @@ private:
   bool alignment_exists_;
   bool pulse_arrived_since_prev_flag_;
 
-  void getFCTimespec(const DJI::OSDK::Telemetry::TimeStamp& stamp_PACKAGE_FC, std::chrono::nanoseconds& time_PACKAGE_FC)
+  static std::chrono::nanoseconds toChronoNsecs(const DJI::OSDK::Telemetry::TimeStamp& stamp_PACKAGE_FC)
   {
     /**
      * NOTE: after checking, it is evident that the field named 'time_ns' in the DJI::OSDK::Telemetry::TimeStamp-
@@ -80,13 +74,13 @@ private:
      * thus used.
     */
     static constexpr int64_t NSECS_PER_USEC{1000};
-    time_PACKAGE_FC = std::chrono::nanoseconds{NSECS_PER_USEC * static_cast<int64_t>(stamp_PACKAGE_FC.time_ns)};
+    return std::chrono::nanoseconds{NSECS_PER_USEC * static_cast<int64_t>(stamp_PACKAGE_FC.time_ns)};
   }
 
-  void getFCTimespec(const DJI::OSDK::Telemetry::SyncTimestamp& stamp_HARDSYNC_FC, std::chrono::nanoseconds& time_HARDSYNC_FC)
+  static std::chrono::nanoseconds toChronoNsecs(const DJI::OSDK::Telemetry::SyncTimestamp& stamp_HARDSYNC_FC)
   {
     static constexpr int64_t NSECS_PER_2P5MSECS{2500000};
-    time_HARDSYNC_FC = std::chrono::nanoseconds{NSECS_PER_2P5MSECS * static_cast<int64_t>(stamp_HARDSYNC_FC.time2p5ms)};
+    return std::chrono::nanoseconds{NSECS_PER_2P5MSECS * static_cast<int64_t>(stamp_HARDSYNC_FC.time2p5ms)};
   }
 };
   
