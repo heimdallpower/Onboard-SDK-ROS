aa09d93c7097cde57800c0d0c3bc3cf1f7705fb5
Magnus Berdal <magnus.berdal@heimdallpower.com>
2025-11-23 12:40:36 +0000

    Use 4 threads

--- dji_sdk/src/main.cpp
+++ dji_sdk/src/main.cpp
@@ -14,8 +14,11 @@
 int main(int argc, char **argv) {
   rclcpp::init(argc, argv);
   auto node{std::make_shared<DJISDKNode>("dji_sdk")};
-#warning("4-threaded async spinner used before. Bring back?");
-  rclcpp::spin(node);
+  rclcpp::executors::MultiThreadedExecutor executor{
+    rclcpp::ExecutorOptions(), 4 // Use 4 threads
+  };
+  executor.add_node(node);
+  executor.spin();
   rclcpp::shutdown();
   return 0;
 }
